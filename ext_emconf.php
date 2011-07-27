e8289ed84a0268c2e51d7ca57fa806f07163e9d9
reubus <reubus@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-07-27 08:44:17 +0000

    initial upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/simplirobots/trunk@50162 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "simplirobots".
+#
+# Auto generated 21-07-2011 22:27
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'simpli robots.txt',
+    'description' => 'Generates a robots.txt fully configarable by Typoscript. Usefull for multidomain and multilingual sites.',
+    'category' => 'fe',
+    'author' => 'Michael Reuber',
+    'author_email' => 'michael.reuber@simpli-cissimus.de',
+    'shy' => '',
+    'dependencies' => '',
+    'conflicts' => 'weeaar_robotstxt,robots_exclusion',
+    'priority' => '',
+    'module' => '',
+    'state' => 'alpha',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'author_company' => '',
+    'version' => '0.0.0',
+    'constraints' => array(
+        'depends' => array(
+        ),
+        'conflicts' => array(
+            'weeaar_robotstxt' => '',
+            'robots_exclusion' => '',
+        ),
+        'suggests' => array(
+            'dd_googlesitemap' => '',
+        ),
+    ),
+    '_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"72df";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"b4c7";s:14:"ext_tables.php";s:4:"4929";s:16:"locallang_db.xml";s:4:"bd93";s:19:"doc/wizard_form.dat";s:4:"840f";s:20:"doc/wizard_form.html";s:4:"c47b";s:33:"pi1/class.tx_simplirobots_pi1.php";s:4:"022e";s:17:"pi1/locallang.xml";s:4:"8f2b";s:20:"static/constants.txt";s:4:"55c9";s:16:"static/setup.txt";s:4:"d41d";s:38:"static/simpli_robots.txt/constants.txt";s:4:"7409";s:34:"static/simpli_robots.txt/setup.txt";s:4:"2ea6";}',
+);
+
+?>
